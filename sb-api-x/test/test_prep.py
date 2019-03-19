428e72faba68eb370daf8a62af9db8f267ddfb01
alex.ramos <alex.ramos@D51860-5530.2d.hq>
2019-03-19 01:59:13 -0700

    renamed top-level folder and removed wordpress stuff

---
+++ sb-api-x/test/test_prep.py
@@ -0,0 +1,14 @@
+import database
+from sys import argv
+import json
+from decimal import Decimal
+
+f = open(argv[1], "r")
+animals = json.loads(f.read(), parse_float=Decimal)
+
+db = database.Database()
+
+for animal in animals:
+    db.prepare(animal)
+    #print(json.dumps(animal, indent=4))
+
--- shelterbuddy-sync/wordpress/test_format.py
+++
@@ -1,8 +0,0 @@
-import wp_article_format
-import json
-
-f = open("animals.json", "r")
-
-data = json.loads(f.read())
-
-print(wp_article_format.animal(data[0], lambda x: "resolve(" + x + ")"))
