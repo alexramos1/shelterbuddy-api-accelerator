a323765c51b4cb83dcab6dc72b9abb4330835756
alex.ramos <alex.ramos@D51860-5530.2d.hq>
2019-03-26 17:50:10 -0700

    script the dynamodb configuration (issue #5)

---
+++ sb-api-x/deploy/deploy_db.py
@@ -0,0 +1,101 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
+
+dynamodb.create_table(
+    TableName='sb-animals',
+    KeySchema=[
+        {
+            'AttributeName': 'Id',
+            'KeyType': 'HASH'  #Partition key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'Id',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'StatusCategory',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'AnimalType',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'LocationKey',
+            'AttributeType': 'S'
+        }
+    ],
+    #BillingMode='PAY_PER_REQUEST',
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    },
+    GlobalSecondaryIndexes=[
+        {
+            'IndexName': 'StatusCategory-AnimalType-index',
+            'KeySchema': [
+                {
+                    'AttributeName': 'StatusCategory',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'AnimalType',
+                    'KeyType': 'RANGE'
+                },
+            ],
+            'Projection': {
+                'ProjectionType': 'ALL'
+            },
+            #'BillingMode': 'PAY_PER_REQUEST',
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        },
+        {
+            'IndexName': 'StatusCategory-LocationKey-index',
+            'KeySchema': [
+                {
+                    'AttributeName': 'StatusCategory',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'LocationKey',
+                    'KeyType': 'RANGE'
+                },
+            ],
+            'Projection': {
+                'ProjectionType': 'ALL'
+            },
+            #'BillingMode': 'PAY_PER_REQUEST',
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        }
+    ],
+)
+
+dynamodb.create_table(
+    TableName='sb-sync',
+    KeySchema=[
+        {
+            'AttributeName': 'hashKey',
+            'KeyType': 'HASH'  #Partition key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'hashKey',
+            'AttributeType': 'S'
+        }
+    ],
+    BillingMode='PROVISIONED',
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    },
+)
