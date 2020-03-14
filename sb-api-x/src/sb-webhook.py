478e5174034292a45eaddd6c79276d0da927fee1
alex.ramos <alex.ramos@D51860-5530.2d.hq>
2020-03-13 20:37:11 -0700

    expand webhook to write to S3, to facilitate regression testing

--- sb-api-x/src/sb-webhook.py
+++ sb-api-x/src/sb-webhook.py
@@ -2,12 +2,24 @@
 import boto3
 import json
 import os
+import uuid
+from datetime import datetime
 
 client = boto3.client('sqs')
 queue  = os.environ['SQS_WEBHOOK']
 
+s3bucket = 'sb-webhook'
+s3client = boto3.client('s3')
+
 def lambda_handler(event, context):
-    client.send_message(QueueUrl=queue, MessageBody=json.dumps(event))
+
+    log = client.send_message(QueueUrl=queue, MessageBody=json.dumps(event))
+    print (log)
+    
+    sequentialPath = datetime.now().strftime('%Y/%m/%d/%H.%M.%S.%f.') + uuid.uuid4().hex + '.json'
+    log = s3client.put_object(Bucket=s3bucket, Key=sequentialPath, Body=json.dumps(event).encode('utf-8', 'replace'))
+    print(log)
+    
     return {
         'statusCode': 200,
         'headers': { },
